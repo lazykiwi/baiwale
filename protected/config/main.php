e7c07ab14ded0c4e67d6f03cac032c5b61f04515
Kiwi <lazykiwi@live.com>
2012-07-16 13:57:08 +0800

    d

--- protected/config/main.php
+++ protected/config/main.php
@@ -46,6 +46,7 @@ return array(
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 			),
 		),
+		
 		'db'=>array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 		),
